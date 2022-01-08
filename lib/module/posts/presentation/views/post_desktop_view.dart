2b7ae6b694a072ee583806a8ef91368bbc7368dc
Julien Deudon <deudon.j@gmail.com>
2022-01-08 11:38:03 +0100

    feat(multi): init desktop views

--- lib/module/posts/presentation/views/post_desktop_view.dart
+++ lib/module/posts/presentation/views/post_desktop_view.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_web/module/posts/data/models/post.dart';
 import 'package:flutter_web/module/posts/presentation/widgets/post_card_widget.dart';
 
-class PostMobileView extends StatelessWidget {
-  const PostMobileView({
+class PostDesktopView extends StatelessWidget {
+  const PostDesktopView({
     Key? key,
     required this.posts,
   }) : super(key: key);
@@ -12,13 +12,12 @@ class PostMobileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return GridView.count(
+      crossAxisCount: posts.length,
       children: posts
-          .map(
-            (post) => PostCardWidget(
-              post: post,
-            ),
-          )
+          .map((post) => PostCardWidget(
+                post: post,
+              ))
           .toList(),
     );
   }
